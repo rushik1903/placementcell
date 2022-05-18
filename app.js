37ad904cf370f53c8a6a4c88980b5ebef39b515f
lahari19 <105435812+lahari19@users.noreply.github.com>
2022-05-18 22:07:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//bodyparser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//http to https redirects
+app.enable('trust proxy');
+app.use((req,res,next)=>{
+    if(req.secure){
+        next();
+    }else{
+        //console.log(process.env.NODE_ENV);
+        
+        if(process.env.NODE_ENV=='production')
+        {
+            res.redirect('https://placements.iiitdm.ac.in'+req.url);
+        }else{
+            next();
+        }
+    }
+});
+
+var home = require("./routes/route.js");
+var erf = require("./routes/erf.js");
+app.use(home);
+app.use(erf);
+
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{console.log(`Server live at port ${PORT}`)});
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Placement Website
+This is a placement website for IIITDM Kancheepuram.
+
+## For developers (Initial Setup)
+- clone this repo
+- always change branch to dev :: git checkout dev
+- nodemon,express installation required 
+- run -> sudo npm i nodemon -g
+- run -> sudo npm i
+# To start server
+- run -> npm run dev
+ ### or 
+- node app.js
