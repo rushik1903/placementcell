92e3e12ead0d7b21960c81da2243a510447059b2
jitinchekka <jitinchekka2@gmail.com>
2022-07-01 21:48:18 +0530

    whyrecruit changes

--- public/scripts/whyRecruit.js
+++ public/scripts/whyRecruit.js
@@ -1,5 +1,5 @@
-var slideIndex1 = 0;
-carousel();
+/*var slideIndex1 = 0;
+// carousel();
 function carousel() {
   var j;
   var x = document.getElementsByClassName("mySlides1");
@@ -10,107 +10,9 @@ function carousel() {
   if (slideIndex1 > x.length) {slideIndex1 = 1}
   x[slideIndex1-1].style.display = "block";
   setTimeout(carousel,2300); // Change image every 2.3 seconds
-}
+}*/
 
 
-function myFunction1() {   
-document.getElementById("coe").classList.toggle("show");
-document.getElementById("edm").classList.remove("show");
-document.getElementById("mdm").classList.remove("show");
-document.getElementById("msm").classList.remove("show");
-}
-
-function myFunction2() {
-document.getElementById("coe").classList.remove("show");   
-document.getElementById("edm").classList.toggle("show");
-document.getElementById("mdm").classList.remove("show");
-document.getElementById("msm").classList.remove("show");
-}
-function myFunction3() {   
-document.getElementById("mdm").classList.toggle("show");
-document.getElementById("msm").classList.remove("show");
-document.getElementById("edm").classList.remove("show");
-document.getElementById("coe").classList.remove("show");   
-}
-
-function myFunction4() {   
-document.getElementById("msm").classList.toggle("show");
-document.getElementById("edm").classList.remove("show");
-document.getElementById("coe").classList.remove("show");  
-document.getElementById("mdm").classList.remove("show");
-
-}
-
-
-function myFunction5() {   
-document.getElementById("ced").classList.toggle("show");
-document.getElementById("evd").classList.remove("show");
-document.getElementById("esd").classList.remove("show");
-document.getElementById("mpd").classList.remove("show");
-document.getElementById("mfd").classList.remove("show");
-
-}
-function myFunction6() {   
-  document.getElementById("ced").classList.remove("show");
-document.getElementById("evd").classList.toggle("show");
-document.getElementById("esd").classList.remove("show");
-document.getElementById("mpd").classList.remove("show");
-document.getElementById("mfd").classList.remove("show");
-}
-
-function myFunction7() {   
-  document.getElementById("ced").classList.remove("show");
-  document.getElementById("evd").classList.remove("show");
-document.getElementById("esd").classList.toggle("show");
-document.getElementById("mpd").classList.remove("show");
-document.getElementById("mfd").classList.remove("show");
-}
-function myFunction8() {   
-  document.getElementById("ced").classList.remove("show");
-  document.getElementById("evd").classList.remove("show");
-  document.getElementById("esd").classList.remove("show");
-document.getElementById("mpd").classList.toggle("show");
-document.getElementById("mfd").classList.remove("show");
-}
-
-
-function myFunction9() {   
-  document.getElementById("ced").classList.remove("show");
-  document.getElementById("evd").classList.remove("show");
-  document.getElementById("esd").classList.remove("show");
-  document.getElementById("mpd").classList.remove("show");
-  document.getElementById("mfd").classList.toggle("show");
-}
-
-
-function myFunction10() {   
-document.getElementById("cds").classList.toggle("show");
-document.getElementById("eds").classList.remove("show");
-document.getElementById("mds").classList.remove("show");
-document.getElementById("smt").classList.remove("show");
-}
-
-
-function myFunction11() {   
-  document.getElementById("cds").classList.remove("show");
-document.getElementById("eds").classList.toggle("show");
-document.getElementById("mds").classList.remove("show");
-document.getElementById("smt").classList.remove("show");
-}
-
-
-function myFunction12() {   
-  document.getElementById("cds").classList.remove("show");
-  document.getElementById("eds").classList.remove("show");
-  document.getElementById("mds").classList.toggle("show");
-  document.getElementById("smt").classList.remove("show");
-}
-
-
-function myFunction13() {   
-  document.getElementById("cds").classList.remove("show");
-  document.getElementById("eds").classList.remove("show");
-  document.getElementById("mds").classList.remove("show");
-  document.getElementById("smt").classList.toggle("show");
-}
-
+const bachelors = document.getElementById("#details1");
+const dd = document.getElementById("details2");
+const masters = document.getElementById("details3");
