e650870f59470fb4ff23704d9ffa3c8128be2852
jitinchekka <jitinchekka2@gmail.com>
2022-07-02 12:18:32 +0530

    completed whyrecruit page

--- public/scripts/whyRecruit.js
+++ public/scripts/whyRecruit.js
@@ -13,6 +13,66 @@ function carousel() {
 }*/
 
 
-const bachelors = document.getElementById("#details1");
+const bachelors = document.getElementById("details1");
 const dd = document.getElementById("details2");
 const masters = document.getElementById("details3");
+
+let btn1 = document.getElementById("btn-1");
+let btn2 = document.getElementById("btn-2");
+let btn3 = document.getElementById("btn-3");
+// Initially display bachelors div and hide the others.
+bachelors.style.display = "block";
+dd.style.display = "none";
+masters.style.display = "none";
+// On clicking btn1, the bachelors div is displayed and the others are hidden.
+btn1.addEventListener("click", function() {
+  bachelors.style.display = "block";
+  dd.style.display = "none";
+  masters.style.display = "none";
+}
+);
+// On clicking btn2, the masters div is displayed and the others are hidden.
+btn2.addEventListener("click", function () {
+  bachelors.style.display = "none";
+  dd.style.display = "block";
+  masters.style.display = "none";
+}
+);
+// On clicking btn3, the masters div is displayed and the others are hidden.
+btn3.addEventListener("click", function () {
+  bachelors.style.display = "none";
+  dd.style.display = "none";
+  masters.style.display = "block";
+}
+);
+// On hovering over the btn1, the bachelors div is displayed and the others are hidden.
+btn1.addEventListener("mouseover", function () {
+  bachelors.style.display = "block";
+  dd.style.display = "none";
+  masters.style.display = "none";
+  btn1.classList.add("active");
+  btn2.classList.remove("active");
+  btn3.classList.remove("active");
+}
+);
+// On hovering over the btn2, the masters div is displayed and the others are hidden.
+btn2.addEventListener("mouseover", function () {
+  bachelors.style.display = "none";
+  dd.style.display = "block";
+  masters.style.display = "none";
+  // add active class to btn2
+  btn2.classList.add("active");
+  btn1.classList.remove("active");
+  btn3.classList.remove("active");
+}
+);
+// On hovering over the btn3, the masters div is displayed and the others are hidden.
+btn3.addEventListener("mouseover", function () {
+  bachelors.style.display = "none";
+  dd.style.display = "none";
+  masters.style.display = "block";
+  btn3.classList.add("active");
+  btn1.classList.remove("active");
+  btn2.classList.remove("active");
+}
+);
