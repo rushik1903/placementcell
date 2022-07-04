e4b4f731d0e0fd05bf0577302dc4007ba29231d2
jitinchekka <jitinchekka2@gmail.com>
2022-07-04 12:57:51 +0530

    changed facilities

--- public/scripts/facilities.js
+++ public/scripts/facilities.js
@@ -1,66 +1,110 @@
-const btn1 = document.getElementById('btn-1');
-const btn2 = document.getElementById('btn-2');
-const btn3 = document.getElementById('btn-3');
-const btn4 = document.getElementById('btn-4');
-const btn5 = document.getElementById('btn-5');
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
 
-btn1.addEventListener('click', displayCard1);
-btn2.addEventListener('click', displayCard2);
-btn3.addEventListener('click', displayCard3);
-btn4.addEventListener('click', displayCard4);
-btn5.addEventListener('click', displayCard5);
+const box1 = document.getElementById("box-1");
+const box2 = document.getElementById("box-2");
+const box3 = document.getElementById("box-3");
+const box4 = document.getElementById("box-4");
+const box5 = document.getElementById("box-5");
+btn1.addEventListener("click", displayCard1);
+btn2.addEventListener("click", displayCard2);
+btn3.addEventListener("click", displayCard3);
+btn4.addEventListener("click", displayCard4);
+btn5.addEventListener("click", displayCard5);
 
-
-
-function displayCard1(){
-  const box1 = document.getElementById('box-1');
-  console.log('clc');
-  if(box1.style.display === 'none'){
-    box1.style.display = 'block';
-  } else {
-    box1.style.display = 'none';
-  }
+let buttons = [box1, box2, box3, box4, box5];
+let boxes = [btn1, btn2, btn3, btn4, btn5];
+function displayCard1() {
+  const box1 = document.getElementById("box-1");
+  console.log("clc");
+  // toogle class active
   
-}
-
-
-function displayCard2(){
-  const box2 = document.getElementById('box-2');
-  console.log('clc');
-  if(box2.style.display === 'none'){
-    box2.style.display = 'block';
+  if (box1.style.display === "none") {
+    // set display of all buttons to none
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+      boxes[i].classList.remove('active');
+      buttons[i].parentElement.style.padding = "0";
+    }
+    box1.style.display = "flex";
+    box1.parentElement.style.padding = "32 0";
   } else {
-    box2.style.display = 'none';
+    box1.style.display = "none";
   }
+  this.classList.toggle("active");
 }
 
-function displayCard3(){
-  const box3 = document.getElementById('box-3');
-  console.log('clc');
-  if(box3.style.display === 'none'){
-    box3.style.display = 'block';
+function displayCard2() {
+  const box2 = document.getElementById("box-2");
+  console.log("clc");
+  
+  if (box2.style.display === "none") {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+      boxes[i].classList.remove('active');
+      buttons[i].parentElement.style.padding = "0";
+    }
+    box2.style.display = "flex";
+    box2.parentElement.style.padding = "32 0";
   } else {
-    box3.style.display = 'none';
+    box2.style.display = "none";
   }
+  this.classList.toggle("active");
 }
 
-function displayCard4(){
-  const box4 = document.getElementById('box-4');
-  console.log('clc');
-  if(box4.style.display === 'none'){
-    box4.style.display = 'block';
+function displayCard3() {
+  const box3 = document.getElementById("box-3");
+  
+  console.log("clc");
+  if (box3.style.display === "none") {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+      boxes[i].classList.remove('active');
+      buttons[i].parentElement.style.padding = "0";
+    }
+    box3.style.display = "flex";
+    box3.parentElement.style.padding = "32 0";
   } else {
-    box4.style.display = 'none';
+    box3.style.display = "none";
   }
+  this.classList.toggle("active");
 }
 
-function displayCard5(){
-  const box5 = document.getElementById('box-5');
-  console.log('clc');
-  if(box5.style.display === 'none'){
-    box5.style.display = 'block';
+function displayCard4() {
+  const box4 = document.getElementById("box-4");
+  
+  console.log("clc");
+  if (box4.style.display === "none") {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+      boxes[i].classList.remove('active');
+      buttons[i].parentElement.style.padding = "0";
+    }
+    box4.style.display = "flex";
+    box4.parentElement.style.padding = "32 0";
   } else {
-    box5.style.display = 'none';
+    box4.style.display = "none";
   }
+  this.classList.toggle("active");
 }
 
+function displayCard5() {
+  const box5 = document.getElementById("box-5");
+  
+  console.log("clc");
+  if (box5.style.display === "none") {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].style.display = "none";
+      boxes[i].classList.remove('active');
+      buttons[i].parentElement.style.padding = "0";
+    }
+    box5.style.display = "flex";
+    box5.parentElement.style.padding = "32 0";
+  } else {
+    box5.style.display = "none";
+  }
+  this.classList.toggle("active");
+}
